528a5f9ef30ef02de4d8ddc5bc9f3462beb30945
Matheus <esdc1984@gmail.com>
2023-05-22 21:42:52 -0300

    .env config

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/website",
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/website`,
 });
 
 api.interceptors.request.use((config) => {
